aaa5a4ac583449b17a6b00b540d4af0b05e02e2e
ThanhDuongg05 <dranky01@gmail.com>
2026-09-12 23:55:34 +0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,38 @@
+# ten="Nam" #kieu chuoi
+# print(ten)
+# so_luong=10 #kieu int
+# print(so_luong)
+# ti_gia=4.5 #kieu double
+# print(ti_gia)
+# is_good=True #kieu boolean
+# print(is_good)
+
+# print("\n")
+
+# ho_Ten="Nguyen Thanh Duong"
+# tuoi=19
+# chieu_cao=1.68
+# is_Student=True
+# print(ho_Ten)
+# print(tuoi)
+# print(chieu_cao)
+# print(is_Student)
+
+print("__________________________________________________")
+#INPUT VA OUTPUT
+# ho_Ten=input("Ten cua ban la gi ? ")
+# print(" Xin Chao " + ho_Ten)
+# tuoi=input("Bao nhieu tuoi ? ")
+# print("Tuoi: "+tuoi)
+# print("ten:" + ho_Ten + "\n"+tuoi + " tuoi" )
+
+print("__________________________________________________")
+#CHUYEN DOI KIEU BIEN
+nam_sinh=input("ban sinh nam bao nhieu? ")
+# print(type(nam_sinh)) kiem tra kieu cua bien
+tuoi=2026-int(nam_sinh) #ep kieu
+print(tuoi)
+
+tien_te=input("Ban co bao nhieu ouce vang: ")
+chi_vang=float(tien_te) * 8,3
+print(chi_vang)
